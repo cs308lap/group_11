073e00816dbbd75f8e50f81ecbcf3b49f50a1fb5
harnaman-hk <harnaman.hk@gmail.com>
2021-08-30 12:32:45 +0530

    non-recursive gcd

---
+++ gcd.cpp
@@ -0,0 +1,16 @@
+#include "functions.h"
+ 
+int gcd(int x, int y)
+{
+    if (x == 0)
+        return y;
+    else if (y == 0)
+        return x;
+    while (x != y) {
+        if (x > y)
+            x = x - y;
+        else
+            y = y - x;
+    }
+    return x;
+}
